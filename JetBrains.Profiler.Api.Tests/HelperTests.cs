31ef45730db874917c211e48a6d85837ec41e251
Mikhail Pilin <mikhail@pilin.name>
2021-06-15 17:03:45 +0200

    Add tests

---
+++ JetBrains.Profiler.Api.Tests/HelperTests.cs
@@ -0,0 +1,36 @@
+using System;
+using NUnit.Framework;
+
+namespace JetBrains.Profiler.Api.Tests
+{
+  public class HelperTests
+  {
+    [Test]
+    public void IdTest()
+    {
+      Assert.AreEqual(Environment.Version.Major, (int) (ushort) (Impl.Helper.Id >> 16));
+      Assert.AreEqual(Environment.Version.Minor, (int) (ushort) Impl.Helper.Id);
+    }
+
+    [Platform("MacOsX")]
+    [Test]
+    public void PlatformMacOsXTest()
+    {
+      Assert.AreEqual(Impl.PlatformId.MacOsX, Impl.Helper.Platform); 
+    }
+
+    [Platform("Linux")]
+    [Test]
+    public void PlatformLinuxTest()
+    {
+      Assert.AreEqual(Impl.PlatformId.Linux, Impl.Helper.Platform); 
+    }
+
+    [Platform("Win32NT")]
+    [Test]
+    public void PlatformWindowsTest()
+    {
+      Assert.AreEqual(Impl.PlatformId.Windows, Impl.Helper.Platform); 
+    }
+  }
+}
