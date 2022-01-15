36462bec6704e9bafebb696965be57effa3ae8c6
Ilia K <ki.stfu@gmail.com>
2022-01-15 18:03:54 +0100

    Load libJetBrains.Profiler.CoreApi.so by its full path but not SONAME cos not supported on Linux musl
    https://www.openwall.com/lists/musl/2022/01/11/4

--- JetBrains.Profiler.Api/Impl/Linux/LinuxHelper.cs
+++ JetBrains.Profiler.Api/Impl/Linux/LinuxHelper.cs
@@ -1,16 +1,12 @@
-using System;
-
+﻿
 namespace JetBrains.Profiler.Api.Impl.Linux
 {
   internal static class LinuxHelper
   {
     public static bool IsCoreApiAlreadyLoaded()
     {
-      var handle = LibDlSo2.dlopen(LibCoreApiSo.LibraryName, RTLD.RTLD_GLOBAL | RTLD.RTLD_LAZY | RTLD.RTLD_NOLOAD);
-      if (handle == IntPtr.Zero)
-        return false;
-      LibDlSo2.dlclose(handle);
-      return true;
+      // Note(k15tfu): dlopen() in musl ignores SONAME, use dl_iterate_phdr().  See https://www.openwall.com/lists/musl/2022/01/11/4.
+      return LibCSo6.Helper.DlIteratePhdrFindLibraryPath(LibCoreApiSo.LibraryName) != null;
     }
   }
 }
